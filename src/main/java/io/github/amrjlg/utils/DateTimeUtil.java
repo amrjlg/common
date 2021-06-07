00afe389ed4fd1f229e58bbab2df4e2951d63616
amrjlg <2899297045@qq.com>
2021-06-07 17:36:56 +0800

    add License

--- src/main/java/io/github/amrjlg/utils/DateTimeUtil.java
+++ src/main/java/io/github/amrjlg/utils/DateTimeUtil.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2021-2021 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.github.amrjlg.utils;
 
 import java.time.LocalDate;
@@ -7,6 +23,11 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.regex.Pattern;
 
+/**
+ * some format of date  , date format and date pattern
+ *
+ * @author jiang
+ */
 public class DateTimeUtil {
 
     public static final Pattern DATA_TIME_PATTERN = Pattern.compile("^((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))\\s([0-1]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$");
--- src/main/java/io/github/amrjlg/utils/HexUtil.java
+++ src/main/java/io/github/amrjlg/utils/HexUtil.java
@@ -1,8 +1,29 @@
+/*
+ *  Copyright (c) 2021-2021 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.github.amrjlg.utils;
 
 import static io.github.amrjlg.utils.ByteUtil.higherHalf;
 import static io.github.amrjlg.utils.ByteUtil.lowerHalf;
 
+/**
+ * hex util
+ *
+ * @author jiang
+ */
 public class HexUtil {
 
     public static final char[] HEX_VALUES = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -32,10 +53,11 @@ public class HexUtil {
     public static String parse(byte... values) {
         return parse(values, " ");
     }
+
     /**
      * The byte array is parsed into hexadecimal strings, each separated by a specific sequence of characters
      *
-     * @param values       fellow byte
+     * @param values    fellow byte
      * @param separator the specific separate char sequence
      * @return hex string
      */
