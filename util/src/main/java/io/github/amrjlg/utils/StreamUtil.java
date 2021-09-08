062afed99a90cfb096bc28241468d067a705e33d
amrjlg <2899297045@qq.com>
2021-09-08 11:33:38 +0800

    primitive array to intStream,longStream,DoubleStream

--- util/src/main/java/io/github/amrjlg/utils/StreamUtil.java
+++ util/src/main/java/io/github/amrjlg/utils/StreamUtil.java
@@ -17,6 +17,17 @@
 
 package io.github.amrjlg.utils;
 
+import io.github.amrjlg.function.ByteToDoubleFunction;
+import io.github.amrjlg.function.ByteToIntFunction;
+import io.github.amrjlg.function.ByteToLongFunction;
+import io.github.amrjlg.function.CharToDoubleFunction;
+import io.github.amrjlg.function.CharToIntFunction;
+import io.github.amrjlg.function.CharToLongFunction;
+import io.github.amrjlg.function.ShortToDoubleFunction;
+import io.github.amrjlg.function.ShortToIntFunction;
+import io.github.amrjlg.function.ShortToLongFunction;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
@@ -24,7 +35,13 @@ import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 import java.util.stream.Collector;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * @author amrjlg
@@ -108,5 +125,98 @@ public class StreamUtil {
 
     public static <T> BinaryOperator<T> first() {
         return (f, l) -> f;
+    }
+
+    /**
+     * int stream
+     *
+     * @param ts       type
+     * @param function map to int function
+     * @param <T>      type of source
+     * @return IntStream
+     */
+    public static <T> IntStream stream(T[] ts, ToIntFunction<T> function) {
+        return Arrays.stream(ArrayUtil.mapToInt(ts, function));
+    }
+
+    public static IntStream stream(byte[] bytes) {
+        return stream(bytes, b -> (int) b);
+    }
+
+    public static IntStream stream(byte[] bytes, ByteToIntFunction function) {
+        return Arrays.stream(ArrayUtil.mapToInt(bytes, function));
+    }
+
+    public static IntStream stream(char[] bytes) {
+        return stream(bytes, b -> (int) b);
+    }
+
+    public static IntStream stream(char[] bytes, CharToIntFunction function) {
+        return Arrays.stream(ArrayUtil.mapToInt(bytes, function));
+    }
+
+    public static IntStream stream(short[] bytes) {
+        return stream(bytes, b -> (int) b);
+    }
+
+    public static IntStream stream(short[] bytes, ShortToIntFunction function) {
+        return Arrays.stream(ArrayUtil.mapToInt(bytes, function));
+    }
+
+    public static <T> LongStream longStream(T[] ts, ToLongFunction<T> function) {
+        return Arrays.stream(ArrayUtil.mapToLong(ts, function));
+    }
+
+    public static LongStream longStream(byte[] bytes) {
+        return longStream(bytes, b -> (long) b);
+    }
+
+    public static LongStream longStream(byte[] bytes, ByteToLongFunction function) {
+        return Arrays.stream(ArrayUtil.mapToLong(bytes, function));
+    }
+
+    public static LongStream longStream(char[] chars) {
+        return longStream(chars, c -> (long) c);
+    }
+
+    public static LongStream longStream(char[] chars, CharToLongFunction function) {
+        return Arrays.stream(ArrayUtil.mapToLong(chars, function));
+    }
+
+    public static LongStream longStream(short[] shorts) {
+        return longStream(shorts, s -> (long) s);
+    }
+
+    public static LongStream longStream(short[] shorts, ShortToLongFunction function) {
+        return Arrays.stream(ArrayUtil.mapToLong(shorts, function));
+    }
+
+
+    public static <T> DoubleStream doubleStream(T[] ts, ToDoubleFunction<T> function) {
+        return Arrays.stream(ArrayUtil.mapToDouble(ts, function));
+    }
+
+    public static DoubleStream doubleStream(byte[] bytes) {
+        return doubleStream(bytes, b -> (double) b);
+    }
+
+    public static DoubleStream doubleStream(byte[] bytes, ByteToDoubleFunction function) {
+        return Arrays.stream(ArrayUtil.mapToDouble(bytes, function));
+    }
+
+    public static DoubleStream doubleStream(char[] chars) {
+        return doubleStream(chars, c -> (double) c);
+    }
+
+    public static DoubleStream doubleStream(char[] bytes, CharToDoubleFunction function) {
+        return Arrays.stream(ArrayUtil.mapToDouble(bytes, function));
+    }
+
+    public static DoubleStream doubleStream(short[] shorts) {
+        return doubleStream(shorts, c -> (double) c);
+    }
+
+    public static DoubleStream doubleStream(short[] bytes, ShortToDoubleFunction function) {
+        return Arrays.stream(ArrayUtil.mapToDouble(bytes, function));
     }
 }
