de5dae369543ee56821da16b04bb9757c0cff226
amrjlg <2899297045@qq.com>
2021-09-09 16:04:47 +0800

    define primitive stream

---
+++ function/src/main/java/io/github/amrjlg/stream/FloatStream.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+import io.github.amrjlg.function.FloatToDoubleFunction;
+import io.github.amrjlg.function.ObjFloatConsumer;
+import io.github.amrjlg.function.FloatBinaryOperator;
+import io.github.amrjlg.function.FloatConsumer;
+import io.github.amrjlg.function.FloatFunction;
+import io.github.amrjlg.function.FloatPredicate;
+import io.github.amrjlg.function.FloatToByteFunction;
+import io.github.amrjlg.function.FloatToCharFunction;
+import io.github.amrjlg.function.FloatToFloatFunction;
+import io.github.amrjlg.function.FloatToIntFunction;
+import io.github.amrjlg.function.FloatToLongFunction;
+import io.github.amrjlg.function.FloatUnaryOperator;
+import io.github.amrjlg.util.OptionalFloat;
+import io.github.amrjlg.util.FloatSummaryStatistics;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+/**
+ * @author amrjlg
+ **/
+public interface FloatStream extends BaseStream<Float,FloatStream> {
+
+    FloatStream map(FloatUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(FloatFunction<? extends U> mapper);
+
+    ByteStream mapToByte(FloatToByteFunction mapper);
+
+    CharStream mapToChar(FloatToCharFunction mapper);
+
+    IntStream mapToInt(FloatToIntFunction mapper);
+
+    LongStream mapToLong(FloatToLongFunction mapper);
+
+    DoubleStream mapToDouble(FloatToDoubleFunction mapper);
+
+    FloatStream flatMap(FloatFunction<? extends FloatStream> mapper);
+
+    FloatStream filter(FloatPredicate predicate);
+
+    FloatStream distinct();
+
+    FloatStream sorted();
+
+    FloatStream peek(FloatConsumer action);
+
+    FloatStream limit(long maxSize);
+
+    FloatStream skip(long n);
+
+    void forEach(FloatConsumer action);
+
+    void forEachOrdered(FloatConsumer action);
+
+    float[] toArray();
+
+    float reduce(float identity, FloatBinaryOperator op);
+
+    OptionalFloat reduce(FloatBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjFloatConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    float sum();
+
+    OptionalFloat min();
+
+    OptionalFloat max();
+
+    long count();
+
+    OptionalFloat average();
+
+    FloatSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(FloatPredicate predicate);
+
+    boolean allMatch(FloatPredicate predicate);
+
+    boolean noneMatch(FloatPredicate predicate);
+
+    OptionalFloat findFirst();
+
+    OptionalFloat findAny();
+
+    Stream<Float> boxed();
+
+    FloatStream sequential();
+
+    FloatStream parallel();
+
+    PrimitiveIterator.OfFloat iterator();
+
+    Spliterator.OfFloat spliterator();
+
+    boolean isParallel();
+
+    FloatStream unordered();
+
+    FloatStream onClose(Runnable closeHandler);
+
+    void close();
+}
---
+++ function/src/main/java/io/github/amrjlg/stream/ShortStream.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+
+import io.github.amrjlg.function.ObjShortConsumer;
+import io.github.amrjlg.function.ShortBinaryOperator;
+import io.github.amrjlg.function.ShortConsumer;
+import io.github.amrjlg.function.ShortFunction;
+import io.github.amrjlg.function.ShortPredicate;
+import io.github.amrjlg.function.ShortToByteFunction;
+import io.github.amrjlg.function.ShortToCharFunction;
+import io.github.amrjlg.function.ShortToFloatFunction;
+import io.github.amrjlg.function.ShortToIntFunction;
+import io.github.amrjlg.function.ShortToLongFunction;
+import io.github.amrjlg.function.ShortUnaryOperator;
+import io.github.amrjlg.util.OptionalShort;
+import io.github.amrjlg.util.ShortSummaryStatistics;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+/**
+ * @author amrjlg
+ **/
+public interface ShortStream extends BaseStream<Short, ShortStream> {
+    ShortStream map(ShortUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(ShortFunction<? extends U> mapper);
+
+    ByteStream mapToByte(ShortToByteFunction mapper);
+
+    CharStream mapToChar(ShortToCharFunction mapper);
+
+    IntStream mapToInt(ShortToIntFunction mapper);
+
+    LongStream mapToLong(ShortToLongFunction mapper);
+
+    FloatStream mapToFloat(ShortToFloatFunction mapper);
+
+    ShortStream flatMap(ShortFunction<? extends ShortStream> mapper);
+
+    ShortStream filter(ShortPredicate predicate);
+
+    ShortStream distinct();
+
+    ShortStream sorted();
+
+    ShortStream peek(ShortConsumer action);
+
+    ShortStream limit(long maxSize);
+
+    ShortStream skip(long n);
+
+    void forEach(ShortConsumer action);
+
+    void forEachOrdered(ShortConsumer action);
+
+    short[] toArray();
+
+    short reduce(short identity, ShortBinaryOperator op);
+
+    OptionalShort reduce(ShortBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjShortConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    short sum();
+
+    OptionalShort min();
+
+    OptionalShort max();
+
+    long count();
+
+    OptionalShort average();
+
+    ShortSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(ShortPredicate predicate);
+
+    boolean allMatch(ShortPredicate predicate);
+
+    boolean noneMatch(ShortPredicate predicate);
+
+    OptionalShort findFirst();
+
+    OptionalShort findAny();
+
+    Stream<Short> boxed();
+
+    ShortStream sequential();
+
+    ShortStream parallel();
+
+    PrimitiveIterator.OfShort iterator();
+
+    Spliterator.OfShort spliterator();
+
+    boolean isParallel();
+
+    ShortStream unordered();
+
+    ShortStream onClose(Runnable closeHandler);
+
+    void close();
+}
