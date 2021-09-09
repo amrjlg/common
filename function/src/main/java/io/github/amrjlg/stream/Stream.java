b11749a7e8fe585b23ff46c5263392ab8acafeae
amrjlg <2899297045@qq.com>
2021-09-09 16:04:59 +0800

    define obj stream

---
+++ function/src/main/java/io/github/amrjlg/stream/Stream.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+import io.github.amrjlg.function.ToByteFunction;
+import io.github.amrjlg.function.ToCharFunction;
+import io.github.amrjlg.function.ToFloatFunction;
+import io.github.amrjlg.function.ToShortFunction;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import java.util.stream.BaseStream;
+import java.util.stream.Collector;
+
+
+/**
+ * @author amrjlg
+ **/
+public interface Stream<T> extends BaseStream<T, Stream<T>> /*extends java.util.stream.Stream<T>*/ {
+
+    ByteStream mapToByte(ToByteFunction<? super T> mapper);
+
+    ShortStream mapToShort(ToShortFunction<? super T> mapper);
+
+    CharStream mapToChar(ToCharFunction<? super T> mapper);
+
+    FloatStream mapToFloat(ToFloatFunction<? super T> mapper);
+
+    IntStream mapToInt(ToIntFunction<? super T> mapper);
+
+    LongStream mapToLong(ToLongFunction<? super T> mapper);
+
+    DoubleStream mapToDouble(ToDoubleFunction<? super T> mapper);
+
+    Stream<T> filter(Predicate<? super T> predicate);
+
+    <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+    <R> Stream<R> flatMap(Function<? super T, ? extends java.util.stream.Stream<? extends R>> mapper);
+
+    IntStream flatMapToInt(Function<? super T, ? extends IntStream> mapper);
+
+    LongStream flatMapToLong(Function<? super T, ? extends LongStream> mapper);
+
+    DoubleStream flatMapToDouble(Function<? super T, ? extends DoubleStream> mapper);
+
+    Stream<T> distinct();
+
+    Stream<T> sorted();
+
+    Stream<T> sorted(Comparator<? super T> comparator);
+
+    Stream<T> peek(Consumer<? super T> action);
+
+    Stream<T> limit(long maxSize);
+
+    Stream<T> skip(long n);
+
+    void forEach(Consumer<? super T> action);
+
+    void forEachOrdered(Consumer<? super T> action);
+
+    Object[] toArray();
+
+    <A> A[] toArray(IntFunction<A[]> generator);
+
+    T reduce(T identity, BinaryOperator<T> accumulator);
+
+    Optional<T> reduce(BinaryOperator<T> accumulator);
+
+    <U> U reduce(U identity, BiFunction<U, ? super T, U> accumulator, BinaryOperator<U> combiner);
+
+    <R> R collect(Supplier<R> supplier, BiConsumer<R, ? super T> accumulator, BiConsumer<R, R> combiner);
+
+    <R, A> R collect(Collector<? super T, A, R> collector);
+
+    Optional<T> min(Comparator<? super T> comparator);
+
+    Optional<T> max(Comparator<? super T> comparator);
+
+    long count();
+
+    boolean anyMatch(Predicate<? super T> predicate);
+
+    boolean allMatch(Predicate<? super T> predicate);
+
+    boolean noneMatch(Predicate<? super T> predicate);
+
+    Optional<T> findFirst();
+
+    Optional<T> findAny();
+
+    Iterator<T> iterator();
+
+    Spliterator<T> spliterator();
+
+    boolean isParallel();
+
+    Stream<T> sequential();
+
+    Stream<T> parallel();
+
+    Stream<T> unordered();
+
+    Stream<T> onClose(Runnable closeHandler);
+
+    void close();
+}
