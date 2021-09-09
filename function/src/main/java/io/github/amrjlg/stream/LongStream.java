9283a729def8cc13041bf2c8119a3adf7e6116d0
amrjlg <2899297045@qq.com>
2021-09-09 16:05:25 +0800

    add long to other primitive type function

---
+++ function/src/main/java/io/github/amrjlg/stream/LongStream.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+import io.github.amrjlg.function.LongToByteFunction;
+import io.github.amrjlg.function.LongToCharFunction;
+import io.github.amrjlg.function.LongToFloatFunction;
+import io.github.amrjlg.function.LongToShortFunction;
+
+import java.util.LongSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.OptionalLong;
+
+import java.util.function.BiConsumer;
+import java.util.function.LongBinaryOperator;
+import java.util.function.LongConsumer;
+import java.util.function.LongFunction;
+import java.util.function.LongPredicate;
+import java.util.function.LongToDoubleFunction;
+import java.util.function.LongToIntFunction;
+import java.util.function.LongUnaryOperator;
+import java.util.function.ObjLongConsumer;
+import java.util.function.Supplier;
+
+
+/**
+ * @author amrjlg
+ **/
+public interface LongStream extends BaseStream<Long, LongStream> {
+
+    LongStream map(LongUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(LongFunction<? extends U> mapper);
+
+    ByteStream mapToByte(LongToByteFunction mapper);
+
+    CharStream mapToChar(LongToCharFunction mapper);
+
+    ShortStream mapToShort(LongToShortFunction mapper);
+
+    IntStream mapToInt(LongToIntFunction mapper);
+
+    FloatStream mapToFloat(LongToFloatFunction mapper);
+
+    DoubleStream mapToDouble(LongToDoubleFunction mapper);
+
+    LongStream flatMap(LongFunction<? extends java.util.stream.LongStream> mapper);
+
+    LongStream filter(LongPredicate predicate);
+
+    LongStream distinct();
+
+    LongStream sorted();
+
+    LongStream peek(LongConsumer action);
+
+    LongStream limit(long maxSize);
+
+    LongStream skip(long n);
+
+    void forEach(LongConsumer action);
+
+    void forEachOrdered(LongConsumer action);
+
+    long[] toArray();
+
+    long reduce(long identity, LongBinaryOperator op);
+
+    OptionalLong reduce(LongBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjLongConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    long sum();
+
+    OptionalLong min();
+
+    OptionalLong max();
+
+    long count();
+
+    OptionalDouble average();
+
+    LongSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(LongPredicate predicate);
+
+    boolean allMatch(LongPredicate predicate);
+
+    boolean noneMatch(LongPredicate predicate);
+
+    OptionalLong findFirst();
+
+    OptionalLong findAny();
+
+    DoubleStream asDoubleStream();
+
+    Stream<Long> boxed();
+
+    LongStream sequential();
+
+    LongStream parallel();
+
+    PrimitiveIterator.OfLong iterator();
+
+    Spliterator.OfLong spliterator();
+
+    boolean isParallel();
+
+    LongStream unordered();
+
+    LongStream onClose(Runnable closeHandler);
+
+    void close();
+}
