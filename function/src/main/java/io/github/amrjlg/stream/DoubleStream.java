25ceba5dd1e0cc6637f012d8454c7e196ac992e9
amrjlg <2899297045@qq.com>
2021-09-09 16:04:47 +0800

    define primitive stream

---
+++ function/src/main/java/io/github/amrjlg/stream/DoubleStream.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+import io.github.amrjlg.function.DoubleToByteFunction;
+import io.github.amrjlg.function.DoubleToCharFunction;
+import io.github.amrjlg.function.DoubleToFloatFunction;
+import io.github.amrjlg.function.DoubleToShortFunction;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.function.BiConsumer;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleFunction;
+import java.util.function.DoublePredicate;
+import java.util.function.DoubleToIntFunction;
+import java.util.function.DoubleToLongFunction;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.ObjDoubleConsumer;
+import java.util.function.Supplier;
+
+
+/**
+ * @author amrjlg
+ **/
+public interface DoubleStream extends BaseStream<Double, DoubleStream> {
+    DoubleStream map(DoubleUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(DoubleFunction<? extends U> mapper);
+
+    ByteStream mapToByte(DoubleToByteFunction mapper);
+
+    CharStream mapToChar(DoubleToCharFunction mapper);
+
+    ShortStream mapToShort(DoubleToShortFunction mapper);
+
+    IntStream mapToInt(DoubleToIntFunction mapper);
+
+    LongStream mapToLong(DoubleToLongFunction mapper);
+
+    FloatStream mapToFloat(DoubleToFloatFunction mapper);
+
+    DoubleStream flatMap(DoubleFunction<? extends DoubleStream> mapper);
+
+    DoubleStream filter(DoublePredicate predicate);
+
+    DoubleStream distinct();
+
+    DoubleStream sorted();
+
+    DoubleStream peek(DoubleConsumer action);
+
+    DoubleStream limit(long maxSize);
+
+    DoubleStream skip(long n);
+
+    void forEach(DoubleConsumer action);
+
+    void forEachOrdered(DoubleConsumer action);
+
+    double[] toArray();
+
+    double reduce(double identity, DoubleBinaryOperator op);
+
+    OptionalDouble reduce(DoubleBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjDoubleConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    double sum();
+
+    OptionalDouble min();
+
+    OptionalDouble max();
+
+    long count();
+
+    OptionalDouble average();
+
+    DoubleSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(DoublePredicate predicate);
+
+    boolean allMatch(DoublePredicate predicate);
+
+    boolean noneMatch(DoublePredicate predicate);
+
+    OptionalDouble findFirst();
+
+    OptionalDouble findAny();
+
+    Stream<Double> boxed();
+
+    DoubleStream sequential();
+
+    DoubleStream parallel();
+
+    PrimitiveIterator.OfDouble iterator();
+
+    Spliterator.OfDouble spliterator();
+
+    boolean isParallel();
+
+    DoubleStream unordered();
+
+    DoubleStream onClose(Runnable closeHandler);
+
+    void close();
+}
---
+++ function/src/main/java/io/github/amrjlg/stream/IntStream.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream;
+
+import io.github.amrjlg.function.IntToByteFunction;
+import io.github.amrjlg.function.IntToCharFunction;
+import io.github.amrjlg.function.IntToFloatFunction;
+import io.github.amrjlg.function.IntToShortFunction;
+
+import java.util.IntSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
+import java.util.function.BiConsumer;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
+import java.util.function.IntToDoubleFunction;
+import java.util.function.IntToLongFunction;
+import java.util.function.IntUnaryOperator;
+import java.util.function.ObjIntConsumer;
+import java.util.function.Supplier;
+
+
+/**
+ * @author amrjlg
+ **/
+public interface IntStream extends BaseStream<Integer, IntStream> {
+
+    IntStream map(IntUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(IntFunction<? extends U> mapper);
+
+    ByteStream mapToByte(IntToByteFunction mapper);
+
+    CharStream mapToChar(IntToCharFunction mapper);
+
+    ShortStream mapToShort(IntToShortFunction mapper);
+
+    LongStream mapToLong(IntToLongFunction mapper);
+
+    FloatStream mapToFloat(IntToFloatFunction mapper);
+
+    DoubleStream mapToDouble(IntToDoubleFunction mapper);
+
+    IntStream flatMap(IntFunction<? extends java.util.stream.IntStream> mapper);
+
+    IntStream filter(IntPredicate predicate);
+
+    IntStream distinct();
+
+    IntStream sorted();
+
+    IntStream peek(IntConsumer action);
+
+    IntStream limit(long maxSize);
+
+    IntStream skip(long n);
+
+    void forEach(IntConsumer action);
+
+    void forEachOrdered(IntConsumer action);
+
+    int[] toArray();
+
+    int reduce(int identity, IntBinaryOperator op);
+
+    OptionalInt reduce(IntBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjIntConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    int sum();
+
+    OptionalInt min();
+
+    OptionalInt max();
+
+    long count();
+
+    OptionalDouble average();
+
+    IntSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(IntPredicate predicate);
+
+    boolean allMatch(IntPredicate predicate);
+
+    boolean noneMatch(IntPredicate predicate);
+
+    OptionalInt findFirst();
+
+    OptionalInt findAny();
+
+    LongStream asLongStream();
+
+    DoubleStream asDoubleStream();
+
+    Stream<Integer> boxed();
+
+    IntStream sequential();
+
+    IntStream parallel();
+
+    PrimitiveIterator.OfInt iterator();
+
+    Spliterator.OfInt spliterator();
+
+    boolean isParallel();
+
+    IntStream unordered();
+
+    IntStream onClose(Runnable closeHandler);
+
+    void close();
+}
