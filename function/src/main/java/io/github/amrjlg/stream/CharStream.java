e4a3dec98989b8c3cebd7ecf30be8912f786e672
amrjlg <2899297045@qq.com>
2021-09-10 09:41:24 +0800

    CharStream

--- function/src/main/java/io/github/amrjlg/stream/CharStream.java
+++ function/src/main/java/io/github/amrjlg/stream/CharStream.java
@@ -17,8 +17,105 @@
 
 package io.github.amrjlg.stream;
 
+import io.github.amrjlg.function.CharBinaryOperator;
+import io.github.amrjlg.function.CharConsumer;
+import io.github.amrjlg.function.CharFunction;
+import io.github.amrjlg.function.CharPredicate;
+import io.github.amrjlg.function.CharToByteFunction;
+import io.github.amrjlg.function.CharToDoubleFunction;
+import io.github.amrjlg.function.CharToFloatFunction;
+import io.github.amrjlg.function.CharToIntFunction;
+import io.github.amrjlg.function.CharToLongFunction;
+import io.github.amrjlg.function.CharUnaryOperator;
+import io.github.amrjlg.function.ObjCharConsumer;
+import io.github.amrjlg.util.CharSummaryStatistics;
+import io.github.amrjlg.util.OptionalChar;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
 /**
  * @author amrjlg
  **/
-public interface CharStream extends BaseStream<Character,CharStream>{
+public interface CharStream extends BaseStream<Character, CharStream> {
+
+    CharStream map(CharUnaryOperator mapper);
+
+    <U> Stream<U> mapToObj(CharFunction<? extends U> mapper);
+
+    ByteStream mapToByte(CharToByteFunction mapper);
+
+    IntStream mapToInt(CharToIntFunction mapper);
+
+    LongStream mapToLong(CharToLongFunction mapper);
+
+    FloatStream mapToFloat(CharToFloatFunction mapper);
+
+    DoubleStream mapToDouble(CharToDoubleFunction mapper);
+
+    CharStream flatMap(CharFunction<? extends CharStream> mapper);
+
+    CharStream filter(CharPredicate predicate);
+
+    CharStream distinct();
+
+    CharStream sorted();
+
+    CharStream peek(CharConsumer action);
+
+    CharStream limit(long maxSize);
+
+    CharStream skip(long n);
+
+    void forEach(CharConsumer action);
+
+    void forEachOrdered(CharConsumer action);
+
+    short[] toArray();
+
+    short reduce(short identity, CharBinaryOperator op);
+
+    OptionalChar reduce(CharBinaryOperator op);
+
+    <R> R collect(Supplier<R> supplier, ObjCharConsumer<R> accumulator, BiConsumer<R, R> combiner);
+
+    short sum();
+
+    OptionalChar min();
+
+    OptionalChar max();
+
+    long count();
+
+    OptionalChar average();
+
+    CharSummaryStatistics summaryStatistics();
+
+    boolean anyMatch(CharPredicate predicate);
+
+    boolean allMatch(CharPredicate predicate);
+
+    boolean noneMatch(CharPredicate predicate);
+
+    OptionalChar findFirst();
+
+    OptionalChar findAny();
+
+    Stream<Character> boxed();
+
+    CharStream sequential();
+
+    CharStream parallel();
+
+    PrimitiveIterator.OfChar iterator();
+
+    Spliterator.OfChar spliterator();
+
+    boolean isParallel();
+
+    CharStream unordered();
+
+    CharStream onClose(Runnable closeHandler);
+
+    void close();
 }
