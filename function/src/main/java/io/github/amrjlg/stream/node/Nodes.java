05d1eeb18a2fc4faac3b065f4f4b4ecfbcc921bf
amrjlg <2899297045@qq.com>
2021-09-13 10:06:53 +0800

    define empty node

--- function/src/main/java/io/github/amrjlg/stream/node/Nodes.java
+++ function/src/main/java/io/github/amrjlg/stream/node/Nodes.java
@@ -18,11 +18,17 @@
 package io.github.amrjlg.stream.node;
 
 import io.github.amrjlg.function.ByteConsumer;
+import io.github.amrjlg.function.CharConsumer;
+import io.github.amrjlg.function.FloatConsumer;
+import io.github.amrjlg.function.ShortConsumer;
 import io.github.amrjlg.stream.iterator.Spliterator;
 import io.github.amrjlg.stream.iterator.Spliterators;
 
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.IntConsumer;
 import java.util.function.IntFunction;
+import java.util.function.LongConsumer;
 
 /**
  * @author amrjlg
@@ -43,11 +49,11 @@ public class Nodes {
     @SuppressWarnings("rawtypes")
     private static final Node EMPTY_NODE = new Nodes.EmptyNode.OfRef();
     private static final Node.OfByte EMPTY_BYTE_NODE = new Nodes.EmptyNode.OfByte();
-//    private static final Node.OfInt EMPTY_INT_NODE = new Nodes.EmptyNode.OfInt();
-//    private static final Node.OfInt EMPTY_INT_NODE = new Nodes.EmptyNode.OfInt();
-//    private static final Node.OfInt EMPTY_INT_NODE = new Nodes.EmptyNode.OfInt();
-//    private static final Node.OfLong EMPTY_LONG_NODE = new Nodes.EmptyNode.OfLong();
-//    private static final Node.OfDouble EMPTY_DOUBLE_NODE = new Nodes.EmptyNode.OfDouble();
+    private static final Node.OfShort EMPTY_SHORT_NODE = new Nodes.EmptyNode.OfShort();
+    private static final Node.OfChar EMPTY_CHAR_NODE = new Nodes.EmptyNode.OfChar();
+    private static final Node.OfInt EMPTY_INT_NODE = new Nodes.EmptyNode.OfInt();
+    private static final Node.OfLong EMPTY_LONG_NODE = new Nodes.EmptyNode.OfLong();
+    private static final Node.OfDouble EMPTY_DOUBLE_NODE = new Nodes.EmptyNode.OfDouble();
 
     public static <T> Node.Builder<T> builder(long size, IntFunction<T[]> generator) {
         return null;
@@ -127,5 +133,94 @@ public class Nodes {
             }
 
         }
+
+        private static final class OfShort extends EmptyNode<Short, short[], ShortConsumer>
+                implements Node.OfShort {
+
+            @Override
+            public Spliterator.OfShort spliterator() {
+                return null;
+            }
+
+            @Override
+            public short[] asPrimitiveArray() {
+                return new short[0];
+            }
+
+        }
+
+        private static final class OfChar extends EmptyNode<Character, char[], CharConsumer>
+                implements Node.OfChar {
+
+            @Override
+            public Spliterator.OfChar spliterator() {
+                return null;
+            }
+
+            @Override
+            public char[] asPrimitiveArray() {
+                return new char[0];
+            }
+
+        }
+
+        private static final class OfInt extends EmptyNode<Integer, int[], IntConsumer>
+                implements Node.OfInt {
+
+            @Override
+            public Spliterator.OfInt spliterator() {
+                return null;
+            }
+
+            @Override
+            public int[] asPrimitiveArray() {
+                return new int[0];
+            }
+
+        }
+
+        private static final class OfLong extends EmptyNode<Long, long[], LongConsumer>
+                implements Node.OfLong {
+
+            @Override
+            public Spliterator.OfLong spliterator() {
+                return null;
+            }
+
+            @Override
+            public long[] asPrimitiveArray() {
+                return new long[0];
+            }
+
+        }
+        private static final class OfFloat extends EmptyNode<Float, float[], FloatConsumer>
+                implements Node.OfFloat {
+
+            @Override
+            public Spliterator.OfFloat spliterator() {
+                return null;
+            }
+
+            @Override
+            public float[] asPrimitiveArray() {
+                return new float[0];
+            }
+
+        }
+        private static final class OfDouble extends EmptyNode<Double, double[], DoubleConsumer>
+                implements Node.OfDouble {
+
+            @Override
+            public Spliterator.OfDouble spliterator() {
+                return null;
+            }
+
+            @Override
+            public double[] asPrimitiveArray() {
+                return new double[0];
+            }
+
+        }
+
     }
 }
