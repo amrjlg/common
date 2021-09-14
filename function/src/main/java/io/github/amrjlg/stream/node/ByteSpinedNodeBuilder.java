f79581f2fd0fc5eada5028bff2ff2d5c1be362c4
amrjlg <2899297045@qq.com>
2021-09-14 11:35:46 +0800

    add ByteSpinedNodeBuilder

---
+++ function/src/main/java/io/github/amrjlg/stream/node/ByteSpinedNodeBuilder.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream.node;
+
+import io.github.amrjlg.function.ByteConsumer;
+import io.github.amrjlg.stream.buffer.SpinedBuffer;
+import io.github.amrjlg.stream.iterator.Spliterator;
+
+
+/**
+ * @author amrjlg
+ **/
+public class ByteSpinedNodeBuilder
+        extends SpinedBuffer.OfByte
+        implements NodeBuilder.OfByte, Node.OfByte {
+
+    private boolean building = false;
+
+    @Override
+    public Spliterator.OfByte spliterator() {
+        assert !building : "during building";
+        return super.spliterator();
+    }
+
+    @Override
+    public void forEach(ByteConsumer consumer) {
+        assert !building : "during building";
+        super.forEach(consumer);
+    }
+
+    //
+    @Override
+    public void begin(long size) {
+        assert !building : "was already building";
+        building = true;
+        clear();
+        ensureCapacity(size);
+    }
+
+    @Override
+    public void accept(byte i) {
+        assert building : "not building";
+        super.accept(i);
+    }
+
+
+    @Override
+    public void end() {
+        assert building : "was not building";
+        building = false;
+        // @@@ check begin(size) and size
+    }
+
+    @Override
+    public void copyInto(byte[] array, int offset) throws IndexOutOfBoundsException {
+        assert !building : "during building";
+        super.copyInto(array, offset);
+    }
+
+    @Override
+    public byte[] asPrimitiveArray() {
+        assert !building : "during building";
+        return super.asPrimitiveArray();
+    }
+
+    @Override
+    public Node.OfByte build() {
+        assert !building : "during building";
+        return this;
+    }
+}
