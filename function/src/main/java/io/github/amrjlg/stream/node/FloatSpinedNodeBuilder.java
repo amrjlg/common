4b6ef75abd2e56952ea547907d8e7861e8ad98c0
amrjlg <2899297045@qq.com>
2021-09-14 11:36:18 +0800

    add FloatSpinedNodeBuilder

---
+++ function/src/main/java/io/github/amrjlg/stream/node/FloatSpinedNodeBuilder.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream.node;
+
+import io.github.amrjlg.function.FloatConsumer;
+import io.github.amrjlg.stream.buffer.SpinedBuffer;
+import io.github.amrjlg.stream.iterator.Spliterator;
+
+
+/**
+ * @author amrjlg
+ **/
+public class FloatSpinedNodeBuilder
+        extends SpinedBuffer.OfFloat
+        implements NodeBuilder.OfFloat, Node.OfFloat {
+
+    private boolean building = false;
+
+    @Override
+    public Spliterator.OfFloat spliterator() {
+        assert !building : "during building";
+        return super.spliterator();
+    }
+
+    @Override
+    public void forEach(FloatConsumer consumer) {
+        assert !building : "during building";
+        super.forEach(consumer);
+    }
+
+    //
+    @Override
+    public void begin(long size) {
+        assert !building : "was already building";
+        building = true;
+        clear();
+        ensureCapacity(size);
+    }
+
+    @Override
+    public void accept(float value) {
+        assert building : "not building";
+        super.accept(value);
+    }
+
+
+    @Override
+    public void end() {
+        assert building : "was not building";
+        building = false;
+        // @@@ check begin(size) and size
+    }
+
+    @Override
+    public void copyInto(float[] array, int offset) throws IndexOutOfBoundsException {
+        assert !building : "during building";
+        super.copyInto(array, offset);
+    }
+
+    @Override
+    public float[] asPrimitiveArray() {
+        assert !building : "during building";
+        return super.asPrimitiveArray();
+    }
+
+    @Override
+    public Node.OfFloat build() {
+        assert !building : "during building";
+        return this;
+    }
+}
