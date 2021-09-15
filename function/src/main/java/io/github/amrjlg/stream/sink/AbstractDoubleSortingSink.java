6c9cfcb1d28d0991f712ea1e3514782d79faa6f6
amrjlg <2899297045@qq.com>
2021-09-15 11:21:07 +0800

    double sort sink

---
+++ function/src/main/java/io/github/amrjlg/stream/sink/AbstractDoubleSortingSink.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream.sink;
+
+import io.github.amrjlg.stream.Sink;
+
+public abstract class AbstractDoubleSortingSink extends Sink.ChainedDouble<Double> {
+    protected boolean cancellationRequested;
+
+    AbstractDoubleSortingSink(Sink<? super Double> downstream) {
+        super(downstream);
+    }
+
+    @Override
+    public final boolean cancellationRequested() {
+        cancellationRequested = true;
+        return false;
+    }
+}
