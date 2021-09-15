e5912f84db549a7b868081de212e8491d50cb3af
amrjlg <2899297045@qq.com>
2021-09-15 11:19:38 +0800

    float sort sink

---
+++ function/src/main/java/io/github/amrjlg/stream/sink/SizedFloatSortingSink.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2021-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package io.github.amrjlg.stream.sink;
+
+import io.github.amrjlg.stream.Sink;
+import io.github.amrjlg.stream.node.Nodes;
+
+import java.util.Arrays;
+
+/**
+ * @author amrjlg
+ **/
+public class SizedFloatSortingSink extends AbstractFloatSortingSink {
+    private float[] array;
+    private int offset;
+
+
+    SizedFloatSortingSink(Sink<? super Float> downstream) {
+        super(downstream);
+    }
+
+    @Override
+    public void begin(long size) {
+        Nodes.maxArraySize(size);
+        array = new float[(int) size];
+        offset = 0;
+    }
+
+    @Override
+    public void end() {
+        Arrays.sort(array, 0, offset);
+        downstream.begin(offset);
+        if (!cancellationRequested) {
+            for (int i = 0; i < offset; i++) {
+                downstream.accept(array[i]);
+            }
+        } else {
+            for (int i = 0; i < offset && !downstream.cancellationRequested(); i++) {
+                downstream.accept(array[i]);
+            }
+        }
+        downstream.end();
+        array = null;
+    }
+
+    @Override
+    public void accept(float value) {
+        array[offset++] = value;
+    }
+}
