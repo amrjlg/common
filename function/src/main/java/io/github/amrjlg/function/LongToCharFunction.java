53fc544ae6b55d5b27469a605de823c3e8b80556
amrjlg <2899297045@qq.com>
2021-09-24 13:36:57 +0800

    method rename

--- function/src/main/java/io/github/amrjlg/function/LongToCharFunction.java
+++ function/src/main/java/io/github/amrjlg/function/LongToCharFunction.java
@@ -21,5 +21,5 @@ package io.github.amrjlg.function;
  * @author amrjlg
  **/
 public interface LongToCharFunction {
-    char applyAsByte(long value);
+    char applyAsChar(long value);
 }
--- function/src/main/java/io/github/amrjlg/function/LongToShortFunction.java
+++ function/src/main/java/io/github/amrjlg/function/LongToShortFunction.java
@@ -21,5 +21,5 @@ package io.github.amrjlg.function;
  * @author amrjlg
  **/
 public interface LongToShortFunction {
-    short applyAsByte(long value);
+    short applyAsShort(long value);
 }
