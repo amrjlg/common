37da6ecc9adc9fba8d019daeb8b149d2c0267b58
amrjlg <2899297045@qq.com>
2022-01-05 18:01:04 +0800

    优化 date util

--- common-util/src/main/java/io/github/amrjlg/utils/DateUtil.java
+++ common-util/src/main/java/io/github/amrjlg/utils/DateUtil.java
@@ -1,23 +1,27 @@
 /*
- * Copyright (c) 2021-2021 the original author or authors.
+ *  Copyright (c) 2021-2022 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package io.github.amrjlg.utils;
 
-import java.time.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
@@ -32,7 +36,9 @@ import java.util.Locale;
  **/
 public class DateUtil {
 
-    public static java.util.regex.Pattern date_pattern = java.util.regex.Pattern.compile(Pattern.DEFAULT_DATE_PATTERN);
+    public static java.util.regex.Pattern DATE_PATTERN = java.util.regex.Pattern.compile(Pattern.DEFAULT_DATE_PATTERN);
+    public static java.util.regex.Pattern TIME_PATTERN = java.util.regex.Pattern.compile(Pattern.DEFAULT_TIME_PATTERN);
+    public static java.util.regex.Pattern DATA_TIME_PATTERN = java.util.regex.Pattern.compile(Pattern.DEFAULT_DATE_TIME_PATTERN);
 
     public interface Pattern {
         String YEAR = "yyyy";
@@ -45,6 +51,8 @@ public class DateUtil {
         String MINUTE = "mm";
         String SECOND = "ss";
 
+        String MILLISECONDS = "SSS";
+
         String TIME_CONCAT = ":";
 
         String DEFAULT_DATE_PATTERN = YEAR + DATE_CONCAT + MONTH + DATE_CONCAT + DAY;
@@ -55,12 +63,16 @@ public class DateUtil {
         String DEFAULT_DATE_TIME_PATTERN = DEFAULT_DATE_PATTERN + StringUtil.SPACE + DEFAULT_TIME_PATTERN;
         String DEFAULT_DATE_TIME_12_PATTERN = DEFAULT_DATE_PATTERN + StringUtil.SPACE + DEFAULT_TIME_12_PATTERN;
 
+        String DEFAULT_DATE_TIME_MILLISECONDS = DEFAULT_DATE_TIME_PATTERN + "." + MILLISECONDS;
+        String DEFAULT_DATE_TIME_MILLISECONDS_NO_SEPARATOR = YEAR + MONTH + DAY + HOUR + MINUTE + SECOND + MILLISECONDS;
+
         String YEAR_MONTH_DAY = YEAR + MONTH + DAY + HOUR + MINUTE + SECOND;
         String HOUR_MINUTE_SECOND = HOUR + MINUTE + SECOND;
         String YEAR_MONTH_DAY_HOUR_MINUTE_SECOND = YEAR_MONTH_DAY + HOUR_MINUTE_SECOND;
     }
 
     public static class Formatter {
+        public static final DateTimeFormatter DEFAULT_TIME_FORMATTER = DateTimeFormatter.ofPattern(Pattern.DEFAULT_TIME_PATTERN);
         public static final DateTimeFormatter DEFAULT_DATE_FORMATTER = builder()
                 .appendValue(ChronoField.YEAR, 4)
                 .appendLiteral(Pattern.DATE_CONCAT)
@@ -81,6 +93,9 @@ public class DateUtil {
                 .appendLiteral(Pattern.TIME_CONCAT)
                 .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
                 .toFormatter();
+
+        public static final DateTimeFormatter DEFAULT_DATE_TIME_MILLISECONDS_FORMATTER = DateTimeFormatter.ofPattern(Pattern.DEFAULT_DATE_TIME_MILLISECONDS);
+
         public static final DateTimeFormatter DEFAULT_DATE_TIME_12_FORMATTER = DateTimeFormatter.ofPattern(Pattern.DEFAULT_DATE_TIME_12_PATTERN);
 
         public static final DateTimeFormatter YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_FORMATTER = builder()
@@ -109,6 +124,61 @@ public class DateUtil {
             return new DateTimeFormatterBuilder();
         }
     }
+
+    /**
+     * 是否是时间 HH:mm:ss
+     *
+     * @param time 时间字符串
+     * @return boolean
+     */
+    public static boolean isTime(String time) {
+        return TIME_PATTERN.matcher(time).matches();
+    }
+
+    public static boolean isTime(String time, String pattern) {
+        return isTime(time, java.util.regex.Pattern.compile(pattern));
+    }
+
+    public static boolean isTime(String time, java.util.regex.Pattern pattern) {
+        return pattern.matcher(time).matches();
+    }
+
+    /**
+     * 是否是时间 yyyy-MM-dd
+     *
+     * @param date 日期字符串
+     * @return boolean
+     */
+    public static boolean isDate(String date) {
+        return DATE_PATTERN.matcher(date).matches();
+    }
+
+    public static boolean isDate(String date, String pattern) {
+        return isDate(date, java.util.regex.Pattern.compile(pattern));
+    }
+
+    public static boolean isDate(String date, java.util.regex.Pattern pattern) {
+        return pattern.matcher(date).matches();
+    }
+
+    /**
+     * 是否是时间 yyyy-MM-dd HH:mm:ss
+     *
+     * @param dateTime 日期时间字符串
+     * @return boolean
+     */
+    public static boolean isDateTime(String dateTime) {
+        return DATA_TIME_PATTERN.matcher(dateTime).matches();
+    }
+
+    public static boolean isDateTime(String dateTime, String pattern) {
+        return isDateTime(dateTime, java.util.regex.Pattern.compile(pattern));
+    }
+
+    public static boolean isDateTime(String dateTime, java.util.regex.Pattern pattern) {
+        return pattern.matcher(dateTime).matches();
+    }
+
 
     public static Date toDate(LocalDateTime dateTime) {
         Instant instant = ZonedDateTime.of(dateTime, ZoneId.systemDefault()).toInstant();
@@ -197,5 +267,34 @@ public class DateUtil {
     public static boolean checkDay(int day) {
         return ChronoField.DAY_OF_MONTH.range().isValidValue(day);
     }
+
+    public static String time(LocalTime time) {
+        return Formatter.DEFAULT_TIME_FORMATTER.format(time);
+    }
+
+    public static String time(LocalDate date) {
+        return "00:00:00";
+    }
+
+    public static String time(LocalDateTime dateTime) {
+        return time(dateTime.toLocalTime());
+    }
+
+    public static String date(LocalDate date) {
+        return Formatter.DEFAULT_DATE_FORMATTER.format(date);
+    }
+
+    public static String date(LocalDateTime dateTime) {
+        return date(dateTime.toLocalDate());
+    }
+
+    public static String dateTimeMilliseconds(LocalDate date) {
+        return dateTimeMilliseconds(LocalDateTime.of(date, LocalTime.MIN));
+    }
+
+    public static String dateTimeMilliseconds(LocalDateTime dateTime) {
+        return Formatter.DEFAULT_DATE_TIME_MILLISECONDS_FORMATTER.format(dateTime);
+    }
+
 
 }
