1e63d50a41eafe3b8a4e4c30abe8c25106b709f8
amrjlg <2899297045@qq.com>
2022-01-17 15:12:03 +0800

    add DefaultExceptionCodeMessage

---
+++ common-exception/src/main/java/io/github/amrjlg/exception/DefaultExceptionCodeMessage.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.amrjlg.exception;
+
+import io.github.amrjlg.api.CodeMessage;
+
+/**
+ * @author lingjiang
+ */
+public enum DefaultExceptionCodeMessage implements CodeMessage {
+
+    LOGICAL_ERROR(40000, "Logical error, please check your data"),
+
+    SYSTEM_ERROR(50000, "system error, please contact administrator"),
+    ;
+
+    private final int code;
+    private final String message;
+
+    DefaultExceptionCodeMessage(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
