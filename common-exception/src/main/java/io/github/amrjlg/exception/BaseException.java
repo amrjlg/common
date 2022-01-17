fc0ec836decf2f46ea1d3d60e796ba7141e0965f
amrjlg <2899297045@qq.com>
2022-01-17 15:12:19 +0800

    define BaseException with CodeMessage and Handler

---
+++ common-exception/src/main/java/io/github/amrjlg/exception/BaseException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.amrjlg.exception;
+
+import io.github.amrjlg.api.CodeMessage;
+
+/**
+ * @author lingjiang
+ */
+public abstract class BaseException extends RuntimeException implements CodeMessage {
+    private final CodeMessage codeMessage;
+
+    public BaseException(CodeMessage codeMessage) {
+        super(codeMessage.getMessage());
+        this.codeMessage = codeMessage;
+    }
+
+    @Override
+    public String getMessage() {
+        return codeMessage.getMessage();
+    }
+}
